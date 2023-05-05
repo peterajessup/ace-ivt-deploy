5fbb254cda4145bf9af7e1d184a4e777fe3b2f8d
peterajessup <peterajessup@users.noreply.github.com>
2023-05-05 13:07:12 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,6 @@ set -e
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
 bash -x createConfig.sh keystore key.jks config/key.jks
 bash -x createConfig.sh setdbparms scramparms config/setdbparms.txt
-bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
+# bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
 
 oc apply -f yaml/aceIVTDeploy.yaml
