63c547180ffcf305655a173a4b98cfcab200a22a
peterajessup <peterajessup@users.noreply.github.com>
2025-07-30 14:33:08 +1000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,11 @@ oc delete Configuration mqpolicy
 set -e
 
 bash -x createConfig.sh serverconf httpsconf config/server.conf.yaml
-bash -x createConfig.sh keystore key.jks config/key.jks
-bash -x createConfig.sh setdbparms scramparms config/setdbparms.txt
+bash -x createConfig.sh keystore key.kdb config/key.kdb
+bash -x createConfig.sh keystore key.rdb config/key.rdb
+bash -x createConfig.sh keystore key.sth config/key.sth
+bash -x createConfig.sh setdbparms keyparms config/setdbparms.txt
+bash -x createConfig.sh vaultarchive otelarchive config/myisvaultarchive.zip
 # bash -x createConfig.sh policyproject mqpolicy config/MQPolicy.zip
 
 oc apply -f yaml/aceIVTDeploy.yaml
